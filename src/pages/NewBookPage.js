d431582e343e354489b8fe0c5c08a045f2c26c60
uriel-martinez1 <martinezu444@gmail.com>
2024-10-11 22:56:59 -0400

    Created the form for adding new books.

--- src/pages/NewBookPage.js
+++ src/pages/NewBookPage.js
@@ -1,16 +1,74 @@
-import { useContext } from "react";
+import { useState } from "react";
 import { ReadingListContext } from "../store/ReadingListContext";
-import BookCard from "../components/BookCard";
-import { Link } from "react-router-dom";
 
 const NewBookPage = () => {
-    // grab the book from the reading list 
-    const { readingListBooks, setReadingStatus, removeFromReadingList } = useContext(ReadingListContext);
+    // we need to store the new book
+    const [inputs, setInputs] = useState({});
+
+    // handle change
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(inputs);
+    }
 
     // we need a way to filter the books
     return (
         <>
-            <h2>Add Book</h2>
+            <form>
+                <input 
+                    type="text"
+                    name="title"
+                    placeholder="Title"
+                    value={inputs.title || ""}
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text"
+                    name="author"
+                    placeholder="Author"
+                    value={inputs.author || ""}
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text"
+                    name="isbn"
+                    placeholder="ISBN"
+                    value={inputs.isbn || ""}
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text"
+                    name="num-pages"
+                    placeholder="Num Pages"
+                    value={inputs.numPages || ""}
+                    onChange={handleChange}
+                />
+
+                <label>Best seller?</label>
+                <input 
+                    type="checkbox"
+                    name="best-seller"
+                />
+                <label>New release?</label>
+                <input 
+                    type="checkbox"
+                    name="new-release"
+                />
+                <input 
+                    type="text"
+                    name="more-info"
+                    placeholder="More Info Link"
+                    value={inputs.moreInfoLink || ""}
+                    onChange={handleChange}
+                />
+                <button type="submit">Save</button>
+            </form>
         </>
     );
 };
